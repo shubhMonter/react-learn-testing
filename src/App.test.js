2b5fdfab7abaf8cb1e3ed20037dcd91d0d919eab
shubham <shubham.dixit@edelweiss.in>
2022-01-13 11:31:00 +0530

    npm run eject

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import enzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+Enzyme.configure({ adapter: new enzymeAdapter() });
+test("renders learn react link", () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+});
+test("renders non-empty learn react ", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
 });
